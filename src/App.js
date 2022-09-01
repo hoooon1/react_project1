c1ed456f6f8afe0fff8d006ed07a3724f952e933
Jihoon_chang3 <wlgns130587@naver.com>
2022-09-01 17:41:16 +0900

    첫경험

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div class="Header">
+        Header
+      </div>
+
+      <div id="one">
+        <div id="a">
+          <div id ='aa'>
+            결과 그래프
+          </div>
+        </div>
+        <div id="b">
+          당신의 ESG력 (점수)
+        </div>
+      </div>
+
+      <div id="two">
+        <div id="c">
+          E
+        </div>
+        <div id="d">
+          S
+        </div>
+        <div id="e">
+          G
+        </div>
+      </div>
+
+      <div id="three">
+        <div id="f">
+          결과에 대한 (전문가의) 코멘트
+        </div>
+      </div>
+
+      <div id="four">
+        <div id="g">
+          공유하기 버튼 / 저장하기 버튼
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
